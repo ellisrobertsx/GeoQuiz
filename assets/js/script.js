4c21f145abce261555d1c7684ba464cdf4d591f8
ellisrobertsx <ellisrob1998@hotmail.co.uk>
2024-05-19 17:44:08 +0000

    added javascript for buttons to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello!")
+// Get all flag buttons
+const flagButtons = document.querySelectorAll('.flag-buttons button');
+
+// Add click event listener to each flag button
+flagButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Show an alert when the button is clicked
+        alert('You clicked the flag button!');
+    });
+});
+
+// Get all location buttons
+const locationButtons = document.querySelectorAll('.locations-buttons button');
+
+// Add click event listener to each location button
+locationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Show an alert when the button is clicked
+        alert('You clicked the location button!');
+    });
+});
+
+
