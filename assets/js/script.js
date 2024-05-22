85e963d6819978d5cd5f93693a5d103fa949a2b9
ellisrobertsx <ellisrob1998@hotmail.co.uk>
2024-05-22 16:27:37 +0000

    added images for flag - hard questions £

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,25 +61,28 @@ const quizContainer = document.getElementById("quiz-container");
 const flagEasyButton = document.querySelector(".flag-easy");
 const initialContent = document.getElementById("initial-content");
 const flagHardButton = document.querySelector(".flag-hard");
-
+const locationsContent = document.querySelector(".locations");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
+// Event listener for flag easy button
 flagEasyButton.addEventListener("click", () => {
     initialContent.classList.add("hidden");
+    locationsContent.classList.add("hidden"); // Hide locations div
     quizContainer.classList.remove("hidden");
     startQuiz();
-
-
-    flagHardButton.addEventListener("click", () => {
-        initialContent.classList.add("hidden");
-        quizContainer.classList.remove("hidden");
-        startQuiz();
-
-    })
 });
 
+// Event listener for flag hard button
+flagHardButton.addEventListener("click", () => {
+    initialContent.classList.add("hidden");
+    locationsContent.classList.add("hidden"); // Hide locations div
+    quizContainer.classList.remove("hidden");
+    startQuiz();
+});
+
+// Function to start the quiz
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
@@ -88,6 +91,7 @@ function startQuiz() {
     showQuestion();
 }
 
+// Function to display the current question
 function showQuestion() {
     resetState();
     const currentQuestion = questions[currentQuestionIndex];
@@ -106,6 +110,7 @@ function showQuestion() {
     });
 }
 
+// Function to reset the state of the quiz
 function resetState() {
     nextButton.style.display = "none";
     while (answerButtons.firstChild) {
@@ -113,6 +118,7 @@ function resetState() {
     }
 }
 
+// Function to handle the selection of an answer
 function selectAnswer(e) {
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct === "true";
@@ -132,6 +138,7 @@ function selectAnswer(e) {
     }, 2000); // 2 seconds delay
 }
 
+// Function to set the status class of an element
 function setStatusClass(element, correct) {
     clearStatusClass(element);
     if (correct) {
@@ -141,11 +148,13 @@ function setStatusClass(element, correct) {
     }
 }
 
+// Function to clear the status class of an element
 function clearStatusClass(element) {
     element.classList.remove("correct");
     element.classList.remove("wrong");
 }
 
+// Function to move to the next question
 function nextQuestion() {
     currentQuestionIndex++;
     showQuestion();
